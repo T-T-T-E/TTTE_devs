5004882bb989c2e3134544578bc85927bc3d4670
Cristian Colocho <colochocristian72@gmail.com>
2025-09-07 16:54:48 +0000

    agregando conexion Mysql

---
+++ config/db.js
@@ -0,0 +1,17 @@
+// Importa el paquete de MySQL
+const mysql = require('mysql2');
+
+// Crea un pool de conexiones a la base de datos MySQL
+// Un pool de conexiones es más eficiente para manejar múltiples solicitudes.
+const pool = mysql.createPool({
+  host: 'TU_HOST',          // Reemplaza con la dirección de tu servidor de base de datos
+  user: 'TU_USUARIO',       // Reemplaza con tu nombre de usuario de la base de datos
+  password: 'TU_CONTRASENA', // Reemplaza con tu contraseña de la base de datos
+  database: 'ttte_db',       // El nombre de la base de datos que creaste en el script SQL
+  waitForConnections: true,  // Espera si el pool está lleno
+  connectionLimit: 10,       // Número máximo de conexiones simultáneas en el pool
+  queueLimit: 0              // El pool no tiene límite de solicitudes en la cola
+});
+
+// Exporta el pool de conexiones para que pueda ser usado en otros archivos de la API
+module.exports = pool;
