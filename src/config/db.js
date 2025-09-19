9664235a82cb9a710a8695b12680393b9105bf8c
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-09-19 13:38:10 -0600

    Se arreglo para las variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'TU_HOST',          // Reemplaza con la dirección de tu servidor de base de datos
-  user: 'TU_USUARIO',       // Reemplaza con tu nombre de usuario de la base de datos
-  password: 'TU_CONTRASENA', // Reemplaza con tu contraseña de la base de datos
-  database: 'ttte_db',       // El nombre de la base de datos que creaste en el script SQL
+  host: process.env.HOSTDB,          // Reemplaza con la dirección de tu servidor de base de datos
+  user: process.env.user,       // Reemplaza con tu nombre de usuario de la base de datos
+  password: process.env.password, // Reemplaza con tu contraseña de la base de datos
+  database: process.env.database,       // El nombre de la base de datos que creaste en el script SQL
   waitForConnections: true,  // Espera si el pool está lleno
   connectionLimit: 10,       // Número máximo de conexiones simultáneas en el pool
   queueLimit: 0              // El pool no tiene límite de solicitudes en la cola
